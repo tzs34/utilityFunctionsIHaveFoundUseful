af91714709352f5c8ca6d01c447deac56bd1ae11
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-10-23 09:39:24 +0100

    Added more examples

--- utils/app-utils.js
+++ utils/app-utils.js
@@ -247,6 +247,29 @@ const formatDate = (date) => {
      UP: 38
  }
 
+
+ const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+const addTwoDPZero = number => {
+  let value = Number(number);
+  let res = String(number).split(".");
+
+  if (res.length === 1 || res[1].length < 3) {
+    value = value.toFixed(2);
+  }
+  return value;
+};
+
+const getCountryCurrency = (balance, code) => {
+  let codes = {
+    GB: "£"
+  };
+
+  let sign = codes[code];
+  let amount = addTwoDPZero(balance);
+
+  return `${sign}${amount}`;
+};
  export {
     compose,
     curry,
@@ -267,5 +290,6 @@ const formatDate = (date) => {
     formatTime,
     isEqualDate,
     monthDiff,
-    keyCodes
+    keyCodes,
+    capitalize, addTwoDPZero, getCountryCurrency 
     }
